9e596247df6d63835644199768354f75ffe43c29
CarlaMassat <cm90mdp@gmail.com>
2024-06-13 08:53:10 -0300

    modified model usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,26 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const moment = require('moment');
+const moment = require("moment");
 
-// las mismas propiedades deben coincidir en el front cuando se definen
 const UsuarioSchema = Schema({
   nombre: String,
   apellido: String,
   email: { type: String, unique: true },
   password: String,
   dni: Number,
-  edad: Number,
-  fechaNacimiento: String,
-  telefono: Number,
-  telefonoEmergencia: Number,
-  turno: String,
-  rol: String,
   fechaIngreso: {
     type: Date,
-    default: () => moment().utc().subtract(3, 'hours').toDate()
-  }
-
+    default: () => moment().utc().subtract(3, "hours").toDate(),
+  },
 });
 
-
-module.exports = mongoose.model('Usuario', UsuarioSchema);
+module.exports = mongoose.model("Usuario", UsuarioSchema);
