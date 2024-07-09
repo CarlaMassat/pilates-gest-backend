6ae42e21178a1f18dd8366fa13060ba680f02352
CarlaMassat <cm90mdp@gmail.com>
2024-07-09 12:16:48 -0300

    modified index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors());
 
 // rutas FALTAN RUTA OBTENER, ACTUALIZAR Y ELIMINAR
 app.use('/api/login', require('./routes/login'));
-app.use('/api/turno', require('./routes/turno'))
+app.use('/api/turno', require('./routes/turno'));
+app.use('/api/horario', require('./routes/horario'));
+
 
 app.listen(process.env.PORT, () =>{
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
